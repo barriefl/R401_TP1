78862971c59a8323aeafdbc0f620fe2a92d2f255
barriefl <barriefl@D360-S09>
2025-01-28 08:49:07 +0100

    Ajout du modèle

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,59 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        private int id;
+        private string? nomDevise;
+        private double taux;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string? NomDevise
+        {
+            get
+            {
+                return nomDevise;
+            }
+
+            set
+            {
+                nomDevise = value;
+            }
+        }
+
+        public double Taux
+        {
+            get
+            {
+                return this.taux;
+            }
+
+            set
+            {
+                this.taux = value;
+            }
+        }
+
+        public Devise()
+        {
+        }
+
+        public Devise(int id, string? nomDevise, double taux)
+        {
+            this.Id = id;
+            this.NomDevise = nomDevise;
+            this.Taux = taux;
+        }
+    }
+}
