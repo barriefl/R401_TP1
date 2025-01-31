26cb9bdf33da66cd0fa466a25155e5031a21de34
barriefl <barriefl@D360-S09>
2025-01-31 09:04:30 +0100

    Codage TestInitialize

--- WSConvertisseurTests/Controllers/DevisesControllerTests.cs
+++ WSConvertisseurTests/Controllers/DevisesControllerTests.cs
@@ -14,11 +14,19 @@ namespace WSConvertisseur.Controllers.Tests
     [TestClass()]
     public class DevisesControllerTests
     {
+        private DevisesController controller;
+
+        [TestInitialize]
+        public void InitialisationDesTests()
+        {
+            // Rajouter les initialisations exécutées avant chaque test
+            controller = new DevisesController();
+        }
+
         [TestMethod]
         public void GetById_ExistingIdPassed_ReturnsRightItem()
         {
             // Arrange
-            DevisesController controller = new DevisesController();
             // Act
             var result = controller.GetById(1);
             // Assert
@@ -32,7 +40,6 @@ namespace WSConvertisseur.Controllers.Tests
         public void GetById_UnknownGuidPassed_ReturnsNotFoundResult()
         {
             // Arrange
-            DevisesController controller = new DevisesController();
             // Act
             var result = controller.GetById(4);
             // Assert
@@ -45,7 +52,6 @@ namespace WSConvertisseur.Controllers.Tests
         public void GetAll_ReturnsRightsItems()
         {
             // Arrange
-            DevisesController controller = new DevisesController();
             // Act
             var result = controller.GetAll();
             List<Devise> resultList = result.ToList();
@@ -63,16 +69,15 @@ namespace WSConvertisseur.Controllers.Tests
         public void Post_ValidObjectPassed_ReturnsObject()
         {
             // Arrange
-            DevisesController controller = new DevisesController();
+            Devise devise = new Devise(4, "Nicolas", 2.5);
             // Act
-            var result = controller.Post(new Devise(4, "Nicolas", 2.5));
-            CreatedAtRouteResult routeResult = (CreatedAtRouteResult)result.Result;
-            var deviseRetournee = (Devise)routeResult.Value;
+            var result = controller.Post(devise);
             // Assert
             Assert.IsInstanceOfType(result, typeof(ActionResult<Devise>), "Pas un ActionResult<Devise>"); // Test du type de retour
             Assert.IsInstanceOfType(result.Result, typeof(CreatedAtRouteResult), "Pas un CreatedAtRouteResult"); // Test du type de retour.retour
+            CreatedAtRouteResult routeResult = (CreatedAtRouteResult)result.Result;
             Assert.AreEqual(StatusCodes.Status201Created, routeResult.StatusCode, "Pas de status 201"); // Test de la property StatusCode
-            Assert.AreEqual(routeResult.Value, deviseRetournee, "Pas la même devise"); // Test la devise retournée
+            Assert.AreEqual(devise, (Devise)routeResult.Value, "Pas la même devise"); // Test la devise retournée
         }
 
         //[TestMethod]
@@ -94,19 +99,57 @@ namespace WSConvertisseur.Controllers.Tests
         [TestMethod]
         public void Put_InvalidUpdate_ReturnsBadRequest()
         {
-
+            // Arrange
+            // Act
+            var result = controller.Put(5, new Devise(4, "Nicolas", 2.5));
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult), "Pas un BadRequestResult"); // Test du type de retour
         }
 
         [TestMethod]
         public void Put_InvalidUpdate_ReturnsNotFound()
         {
-
+            // Arrange
+            // Act
+            var result = controller.Put(100, new Devise(100, "Yin-Yang", 5));
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult), "Pas un NotFoundResult"); // Test du type de retour
         }
 
         [TestMethod]
         public void Put_ValidUpdate_ReturnsNoContent()
         {
+            // Arrange
+            // Act
+            var result = controller.Put(3, new Devise(3, "Yen", 120));
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NoContentResult), "Pas un NoContentResult"); // Test du type de retour
+        }
 
+        [TestMethod]
+        public void Delete_NotOk_ReturnsNotFound()
+        {
+            // Arrange
+            // Act
+            var result = controller.Delete(5);
+            // Assert
+            Assert.IsInstanceOfType(result.Result, typeof(NotFoundResult), "Pas un NotFoundResult"); // Test du type de retour
+        }
+
+        [TestMethod]
+        public void Delete_Ok_ReturnsRightItem()
+        {
+            // Arrange
+            // Act
+            var result = controller.Delete(2);
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ActionResult<Devise>), "Pas un ActionResult<Devise>"); // Test du type de retour
+        }
+
+        [TestCleanup]
+        public void NettoyageDesTests()
+        {
+            // Nettoyer les variables, ... après chaque test
         }
     }
 }
